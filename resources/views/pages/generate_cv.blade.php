9ee18e222848157a1e39991fff41e175e15dbb21
Clinton Ezeronye <46147913+clinton341@users.noreply.github.com>
2020-05-15 21:52:05 +0100

    Latest update from bede

--- resources/views/pages/generate_cv.blade.php
+++ resources/views/pages/generate_cv.blade.php
@@ -61,7 +61,7 @@
       <div class="w3-container w3-card w3-white">
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
         <div class="w3-container">
-          <p>Add Education and Certifications <i class="fa fa-plus-circle fa-fw w3-margin-right w3-large w3-text-teal"></i></p>
+          <p>Add Education and Certification <i class="fa fa-plus-circle fa-fw w3-margin-right w3-large w3-text-teal"></i></p>
           <hr>
         </div>
       </div>
