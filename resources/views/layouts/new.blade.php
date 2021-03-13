b22a19c98a1883b6b09b309c06b176fa9bdc6e7b
Chidi Enwereji <chidibede@gmail.com>
2021-03-13 15:58:24 +0100

    fixed heroku not serving js and css

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -9,10 +9,10 @@
 	<title>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Scripts --> 
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto:300');
